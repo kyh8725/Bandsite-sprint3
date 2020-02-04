56430243793d199f27832e79c749d47d0f57b10c
Yoonho Kim <Daniel@Yoonhos-MacBook-Pro.local>
2020-02-03 20:52:09 -0800

    API used, getDate fucntion modified

---
+++ script/form.js
@@ -0,0 +1,170 @@
+let commentArray = [];
+axios
+  .get("https://project-1-api.herokuapp.com/comments?api_key=Daniel")
+  .then(response => {
+    commentArray = response.data;
+    const form = document.querySelector(".conv__join");
+    form.addEventListener("submit", event => {
+      event.preventDefault();
+      let name = event.target.name.value;
+      let timestamp = new Date().getTime();
+      let comment = event.target.comment.value;
+
+      if (name !== "" && comment !== "") {
+        let comObj = {
+          name: name,
+          timestamp: timestamp,
+          comment: comment,
+          img: "../assets/Images/face3.png"
+        };
+        commentArray.push(comObj);
+        document.querySelector(".conv__wrapper").innerText = "";
+
+        for (object of commentArray) {
+          displayComment(object);
+        }
+        event.target.name.value = "";
+        event.target.comment.value = "";
+      } else {
+        window.alert("Please fill both name and comment");
+      }
+    });
+    for (object of commentArray) {
+      displayComment(object);
+    }
+  });
+
+//axios.post("https://project-1-api.herokuapp.com/comments?api_key=Daniel",{});
+
+function createInsertBefore(name, parent, type) {
+  let child = document.createElement(type);
+  child.classList.add(name);
+  let parents = document.querySelector(parent);
+  parents.insertBefore(child, parents.childNodes[0]);
+  return child;
+}
+
+function createAppend(name, parent, type) {
+  let child = document.createElement(type);
+  child.classList.add(name);
+  parent.appendChild(child);
+  return child;
+}
+
+function printDivider() {
+  let wrapper = document.querySelector(".conv__wrapper");
+  let divider = document.createElement("p");
+  divider.classList.add("conv__divider");
+  wrapper.insertBefore(divider, wrapper.childNodes[1]);
+}
+
+//make html structure for conversation section and returns structure object
+function structureHtml() {
+  let firstDiv = createInsertBefore("conv__comment", ".conv__wrapper", "div");
+  let secondDiv = createAppend("conv__comment-left", firstDiv, "div");
+  let img = createAppend("comment-img", secondDiv, "img");
+  let thirdDiv = createAppend("conv__comment-right", firstDiv, "div");
+  let fourthDiv = createAppend("conv__comment-right-top", thirdDiv, "div");
+  let commentName = createAppend(
+    "conv__comment-right-top-name",
+    fourthDiv,
+    "p"
+  );
+  let commentDate = createAppend(
+    "conv__comment-right-top-date",
+    fourthDiv,
+    "p"
+  );
+  let paragraphDiv = createAppend("comment-paragraph", thirdDiv, "div");
+  let paragraph = createAppend(
+    "conv__comment-right-top-paragraph",
+    paragraphDiv,
+    "p"
+  );
+
+  let structure = {
+    img: img,
+    commentName: commentName,
+    commentDate: commentDate,
+    comment: paragraph
+  };
+  return structure;
+}
+
+function getDate(timestamp) {
+  let currentTime = new Date().getTime();
+  let diffMs = currentTime - timestamp;
+  let diffS = diffMs / 1000;
+  let diffM = diffMs / 60000;
+  let diffH = diffMs / 3600000;
+  let diffD = diffMs / 86400000;
+  let diffMo = diffMs / 2629800000;
+  let diffY = diffMs / 31557600000;
+
+  let result = "";
+
+  if (diffY >= 1) {
+    diffY < 2
+      ? (result = Math.floor(diffY) + " year ")
+      : Math.floor(diffY) + " years ";
+    let tempMo = diffMo % 12;
+    tempMo < 2
+      ? (result += Math.floor(tempMo) + " month ago")
+      : (result += Math.floor(tempMo) + " months ago");
+  } else {
+    if (diffMo > 1 && diffMo < 12) {
+      diffMo < 2
+        ? (result = Math.floor(diffMo) + " month ")
+        : (result = Math.floor(diffMo) + " months ");
+      let tempD = diffD % 31;
+      tempD < 2
+        ? (result += Math.floor(tempD) + " day ago")
+        : (result += Math.floor(tempD) + " days ago");
+    } else {
+      // assumming 1 month = 31 days;
+      if (diffD > 1 && diffD < 31) {
+        diffD < 2
+          ? (result = Math.floor(diffD) + " day ")
+          : (result = Math.floor(diffD) + " days ");
+        let tempH = diffH % 24;
+        tempH < 2
+          ? (result += Math.floor(tempH) + " hour ago")
+          : (result += Math.floor(tempH) + " hours ago");
+      } else {
+        if (diffH > 1 && diffH < 24) {
+          diffH < 2
+            ? (result = Math.floor(diffH) + " hour ")
+            : (result = Math.floor(diffH) + " hours ");
+          let tempM = diffM % 60;
+          tempM < 2
+            ? (result += Math.floor(tempM) + " min ago")
+            : (result += Math.floor(tempM) + " mins ago");
+        } else {
+          if (diffM > 0 && diffM < 1) {
+            diffS < 2
+              ? (result = Math.ceil(diffM) + " min ago")
+              : (result = Math.ceil(diffM) + " mins ago");
+            let tempS = diffS % 1000;
+            diffS < 1
+              ? (result = Math.ceil(tempS) + " second ago")
+              : (result = Math.ceil(tempS) + " seconds ago");
+          } else {
+            diffM > 0 && diffM < 2
+              ? (result = Math.floor(diffM) + " min ago")
+              : (result = Math.floor(diffM) + " mins ago");
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
+function displayComment(commentObject) {
+  let commentHtml = structureHtml();
+  commentHtml.commentName.innerText = commentObject.name;
+  commentHtml.commentDate.innerText = getDate(commentObject.timestamp);
+  commentHtml.comment.innerText = commentObject.comment;
+  commentHtml.img.setAttribute("src", "../assets/Images/face3.png");
+  printDivider();
+}
---
+++ script/show.js
@@ -0,0 +1,65 @@
+let showData = [];
+axios
+  .get("https://project-1-api.herokuapp.com/showdates?api_key=daniel")
+  .then(response => {
+    showData = response.data;
+    presentShow();
+  });
+
+function createAdd(name, parent, type) {
+  parent = document.querySelector(parent);
+  let child = document.createElement(type);
+  child.classList.add(name);
+  parent.appendChild(child);
+  return child;
+}
+function createAddShow(name, parent, type) {
+  let child = document.createElement(type);
+  child.classList.add(name);
+  parent.appendChild(child);
+  return child;
+}
+
+//structuring the show table and assigning the values
+function presentShow() {
+  for (obj of showData) {
+    let showContent = createAddShow(
+      "content__right-single",
+      contentRight,
+      "div"
+    );
+    createAddShow("content__show-date-title", showContent, "h3").innerText =
+      "DATE";
+    createAddShow("content__show-date", showContent, "p").innerText = obj.date;
+    createAddShow("content__show-venue-title", showContent, "h3").innerText =
+      "VENUE";
+    createAddShow("content__show-venue", showContent, "p").innerText =
+      obj.place;
+    createAddShow("content__show-location-title", showContent, "h3").innerText =
+      "LOCATION";
+    createAddShow("content__show-location", showContent, "p").innerText =
+      obj.location;
+    createAddShow("content__show-button", showContent, "button").innerText =
+      "BUY TICKETS";
+    createAddShow("content__show-divider", contentRight, "p");
+  }
+}
+
+//structure HTML for header and info title (dates venue location).
+createAdd("content", ".main", "div");
+createAdd("content__left", ".content", "div");
+createAdd("content__header", ".content__left", "div").innerText = "Shows";
+let contentRight = createAdd("content__right", ".content", "div");
+createAdd("content__info-title", ".content__right", "div");
+
+//dates venue locations -- titles invisible in mobile version
+createAdd("content__info-title-date", ".content__info-title", "p").innerText =
+  "DATES";
+createAdd("content__info-title-venue", ".content__info-title", "p").innerText =
+  "VENUE";
+createAdd(
+  "content__info-title-location",
+  ".content__info-title",
+  "p"
+).innerText = "LOCATION";
+createAdd("content__info-title-dummie", ".content__info-title", "p");
