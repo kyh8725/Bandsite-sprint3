77bccb0363250c6084a86775389b944da21d832f
Yoonho Kim <Daniel@Yoonhos-MacBook-Pro.local>
2020-02-05 23:51:21 -0800

    getDate() fixed, getDatePrint() fixed, font - image path fixed

--- script/form.js
+++ script/form.js
@@ -1,79 +1,3 @@
-function createInsertBefore(name, parent, type, itemId) {
-  let child = document.createElement(type);
-  child.classList.add(name);
-  child.id = itemId;
-  let parents = document.querySelector(parent);
-  parents.insertBefore(child, parents.childNodes[0]);
-  return child;
-}
-
-function createAppend(name, parent, type, itemId) {
-  let child = document.createElement(type);
-  child.classList.add(name);
-  child.id = itemId;
-  parent.appendChild(child);
-  if (name === "conv__delete-button") {
-    child.addEventListener("click", event => {
-      event.preventDefault();
-      axios
-        .delete(
-          "https://project-1-api.herokuapp.com/comments/" +
-            child.id +
-            "?api_key=Daniel"
-        )
-        .then(response => {
-          let deletedDiv = document.querySelector(".conv__comment");
-          if (deletedDiv.id === response.data.id) {
-            deletedDiv.style.display = "none";
-          }
-        });
-    });
-  }
-  return child;
-}
-
-function structureHtml(itemId) {
-  let firstDiv = createInsertBefore(
-    "conv__comment",
-    ".conv__wrapper",
-    "div",
-    itemId
-  );
-  let secondDiv = createAppend("conv__comment-left", firstDiv, "div");
-  let img = createAppend("comment-img", secondDiv, "img");
-  let thirdDiv = createAppend("conv__comment-right", firstDiv, "div");
-  let fourthDiv = createAppend("conv__comment-right-top", thirdDiv, "div");
-  let commentName = createAppend(
-    "conv__comment-right-top-name",
-    fourthDiv,
-    "p"
-  );
-  let commentDate = createAppend(
-    "conv__comment-right-top-date",
-    fourthDiv,
-    "p"
-  );
-  let paragraphDiv = createAppend("conv__comment-paragraph", thirdDiv, "div");
-  let paragraph = createAppend(
-    "conv__comment-paragraph-text",
-    paragraphDiv,
-    "p"
-  );
-  createAppend(
-    "conv__delete-button",
-    paragraphDiv,
-    "button",
-    itemId
-  ).innerText = "DELETE";
-  let structure = {
-    img: img,
-    commentName: commentName,
-    commentDate: commentDate,
-    comment: paragraph
-  };
-  return structure;
-}
-
 function getDate(timestamp) {
   let currentTime = new Date().getTime();
   let diffMs = currentTime - timestamp;
@@ -88,50 +12,50 @@ function getDate(timestamp) {
 
   if (diffY >= 1) {
     diffY < 2
-      ? (result = Math.floor(diffY) + " year ")
-      : Math.floor(diffY) + " years ";
+      ? (result = Math.floor(diffY) + " yr ")
+      : (result = Math.floor(diffY) + " yrs ");
     let tempMo = diffMo % 12;
     tempMo < 2
-      ? (result += Math.floor(tempMo) + " month ago")
-      : (result += Math.floor(tempMo) + " months ago");
+      ? (result += tempMo.toFixed(0) + " mth ago")
+      : (result += tempMo.toFixed(0) + " mths ago");
   } else {
     if (diffMo > 1 && diffMo < 12) {
       diffMo < 2
-        ? (result = Math.floor(diffMo) + " month ")
-        : (result = Math.floor(diffMo) + " months ");
+        ? (result = diffMo.toFixed(0) + " mth ")
+        : (result = diffMo.toFixed(0) + " mths ");
       let tempD = diffD % 31;
       tempD < 2
-        ? (result += Math.floor(tempD) + " day ago")
-        : (result += Math.floor(tempD) + " days ago");
+        ? (result += tempD.toFixed(0) + " day ago")
+        : (result += tempD.toFixed(0) + " days ago");
     } else {
       // assumming 1 month = 31 days;
       if (diffD > 1 && diffD < 31) {
         diffD < 2
-          ? (result = Math.floor(diffD) + " day ")
-          : (result = Math.floor(diffD) + " days ");
+          ? (result = diffD.toFixed(0) + " day ")
+          : (result = diffD.toFixed(0) + " days ");
         let tempH = diffH % 24;
         tempH < 2
-          ? (result += Math.floor(tempH) + " hour ago")
-          : (result += Math.floor(tempH) + " hours ago");
+          ? (result += tempH.toFixed(0) + " hr ago")
+          : (result += tempH.toFixed(0) + " hrs ago");
       } else {
         if (diffH > 1 && diffH < 24) {
           diffH < 2
-            ? (result = Math.floor(diffH) + " hour ")
-            : (result = Math.floor(diffH) + " hours ");
+            ? (result = diffH.toFixed(0) + " hr ")
+            : (result = diffH.toFixed(0) + " hrs ");
           let tempM = diffM % 60;
           tempM < 2
-            ? (result += Math.ceil(tempM) + " min ago")
-            : (result += Math.ceil(tempM) + " mins ago");
+            ? (result += Math.floor(tempM) + " min ago")
+            : (result += tempM.toFixed(0) + " mins ago");
         } else {
           if (diffM > 0 && diffM < 1) {
             let tempS = diffS % 1000;
             diffS < 1
               ? (result = Math.ceil(tempS) + " second ago")
-              : (result = Math.ceil(tempS) + " seconds ago");
+              : (result = tempS.toFixed(0) + " seconds ago");
           } else {
             diffM > 0 && diffM < 1.99
-              ? (result = Math.round(diffM) + " min ago")
-              : (result = Math.round(diffM) + " mins ago");
+              ? (result = Math.floor(diffM) + " min ago")
+              : (result = diffM.toFixed(0) + " mins ago");
           }
         }
       }
@@ -140,25 +64,84 @@ function getDate(timestamp) {
   return result;
 }
 
-function displayComment(commentObject) {
-  let commentHtml = structureHtml(commentObject.id);
-  commentHtml.commentName.innerText = commentObject.name;
-  commentHtml.commentDate.innerText = getDate(commentObject.timestamp);
-  commentHtml.comment.innerText = commentObject.comment;
-  commentHtml.img.setAttribute("src", "../assets/Images/face3.png");
+//creates html tag and add it to parent tag as a first element.
+function createBefore(name, parent, type, itemId) {
+  let child = document.createElement(type);
+  child.classList.add(name);
+  child.id = itemId;
+  let parents = document.querySelector(parent);
+  parents.insertBefore(child, parents.childNodes[0]);
+  return child;
+}
+//creates html tag and add it to parent tag.
+function createAfter(name, parent, type, itemId) {
+  let child = document.createElement(type);
+  child.classList.add(name);
+  child.id = itemId;
+  parent.appendChild(child);
+  if (name === "conv__delete-button") {
+    child.addEventListener("click", event => {
+      event.preventDefault();
+      axios
+        .delete(
+          "https://project-1-api.herokuapp.com/comments/" +
+            child.id +
+            "?api_key=Daniel"
+        )
+        .then(response => {
+          const deletedDiv = document.querySelector(".conv__comment");
+          if (deletedDiv.id === response.data.id) {
+            deletedDiv.style.display = "none";
+          }
+        });
+    });
+  }
+  return child;
 }
 
-function printData() {
+function structureHtml(itemId) {
+  let firstDiv = createBefore("conv__comment", ".conv__wrapper", "div", itemId);
+  let secondDiv = createAfter("conv__comment-left", firstDiv, "div");
+  let img = createAfter("comment-img", secondDiv, "img");
+  let thirdDiv = createAfter("conv__comment-right", firstDiv, "div");
+  let fourthDiv = createAfter("conv__comment-right-top", thirdDiv, "div");
+  let commentName = createAfter("conv__comment-right-top-name", fourthDiv, "p");
+  let commentDate = createAfter("conv__comment-right-top-date", fourthDiv, "p");
+  let paraDiv = createAfter("conv__comment-paragraph", thirdDiv, "div");
+  let paragraph = createAfter("conv__comment-paragraph-text", paraDiv, "p");
+  createAfter("conv__delete-button", paraDiv, "button", itemId).innerText =
+    "DELETE";
+  let structure = {
+    img: img,
+    commentName: commentName,
+    commentDate: commentDate,
+    comment: paragraph
+  };
+  return structure;
+}
+
+function displayComment(commentObject) {
+  let html = structureHtml(commentObject.id);
+  html.commentName.innerText = commentObject.name;
+  html.commentDate.innerText = getDate(commentObject.timestamp);
+  html.comment.innerText = commentObject.comment;
+  html.img.src = "./assets/Images/face3.png";
+}
+
+function getDataPrint() {
   axios
     .get("https://project-1-api.herokuapp.com/comments?api_key=Daniel")
     .then(response => {
-      for (object of response.data) {
-        displayComment(object, response.data.id);
+      const commentWrapper = document.querySelector(".conv__wrapper");
+      commentWrapper.innerHTML = "";
+      for (obj of response.data) {
+        displayComment(obj);
       }
     });
+  setTimeout(getDataPrint, 900);
 }
 
-printData();
+getDataPrint();
 const form = document.querySelector(".conv__join");
 form.addEventListener("submit", event => {
   event.preventDefault();
